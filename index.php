d6a90e5ae0ad3b11558f51577d77953591cc071a
carlosdaniel <caelospina@gmail.com>
2020-08-30 11:38:41 -0500

    cambios

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="estilos.css">
+    <link rel="stylesheet" href="Estilos.css">
+    
     <title>Club Deportivo Fénix</title>
 </head>
 <body>
