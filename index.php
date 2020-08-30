b57a8b5ffa473cc42a32aab135e14c7b8e88acb9
carlosdaniel <caelospina@gmail.com>
2020-08-30 18:57:43 -0500

    se agregan hr en los articulos

--- index.php
+++ index.php
@@ -32,23 +32,27 @@
 
     <div class="articulos">
     <H2>SOBRE NOSOTROS:</H2>
+    <hr>
     <article class="art1">
     <h3> DESCRIPCIÓN DEL SITIO WEB</h3>
     <P>Este sitio está orientato a la presentación de un club de fútbol infantil,el cual debido a su crecimiento 
         necesita un salto a la innovación, tanto en sus procesos de inscripción, como en la presentación de su trabajo.</P>
     </article>
+    <hr>
     <article class="art2">
         <h3>¿QUÉ VERA EL USUARIO FINAL?</h3>
         <P> Lo que el usuario final podra ver, es un sitio web con un buen diseño, que le permita una facil navegación. 
             Lo más importante es que los usuarios se sientan cómodos accediendo a las distintas páginas del sitio a las que 
             les corresponda ir. En conclusión, se busca una armonia de diseño y usabilidad.</P>
         </article>
+        <hr>
         <article class="art3">
             <h3>¿PARA QUÉ FUNCIONARA EL SITIO WEB?</h3>
             <P>Basicamente el club necesita que sus procesos de formación deportiva sean presentados de una forma novedosa 
                 y qué mejor manera de hacerlo que con su propio sitio web, además el crecimineto del club requiere que sus procesos 
                 de pago de matriculas e inscripción sean adecuados de una forma virtual. </P>
             </article>
+            <hr>
         </div>
         <div class="emb">
         <iframe class="facebook" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ffenixgirardota2015&tabs=timeline&width=500&height=300&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="500" height="300" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>      
