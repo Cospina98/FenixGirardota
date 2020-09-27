e5da0baee67e2afd7be8460e0246c3a884efb47c
carlosdaniel <caelospina@gmail.com>
2020-09-27 11:28:24 -0500

    Cambios acordados en asesoria

--- index.php
+++ index.php
@@ -14,65 +14,146 @@
 <body>
   
     <nav>
+       
         <ul class="menu">
-            <li><a href="index.php">INICIO</a></li>
+    
+            <li><a href="Fenix.html">INICIO</a></li>
             <li><a href="rutinas.html">RUTINAS</a></li>
             <li><a href="destacado.html">DESTACADO</a></li>
             <li><a href="contacto.html" target="blank">CONTACTO</a></li>
         </ul>
+ </nav>
+ 
+ 
+    <div class="imagen" > </div>
 
-    </nav>
 
-
-    <div class="imagen"></div>
-
-    <div class="articulos">
-    <H2>SOBRE NOSOTROS:</H2>
-    <hr>
-    <article class="art1">
-    <h3> DESCRIPCIÓN DEL SITIO WEB</h3>
+<section clas="articulos">
+    
+        <article class="art "> <h3 style="text-align: center;"> DESCRIPCIÓN DEL SITIO WEB</h3>
     <P>Este sitio está orientato a la presentación de un club de fútbol infantil,el cual debido a su crecimiento 
         necesita un salto a la innovación, tanto en sus procesos de inscripción, como en la presentación de su trabajo.</P>
     </article>
-    <hr>
-    <article class="art2">
-        <h3>¿QUÉ VERA EL USUARIO FINAL?</h3>
+    <article  class="art ">
+        <h3 style="text-align: center;">¿QUÉ VERA EL USUARIO FINAL?</h3>
         <P> Lo que el usuario final podra ver, es un sitio web con un buen diseño, que le permita una facil navegación. 
             Lo más importante es que los usuarios se sientan cómodos accediendo a las distintas páginas del sitio a las que 
             les corresponda ir. En conclusión, se busca una armonia de diseño y usabilidad.</P>
         </article>
-        <hr>
-        <article class="art3">
-            <h3>¿PARA QUÉ FUNCIONARA EL SITIO WEB?</h3>
+        <article  class="art ">
+            <h3 style="text-align: center;">¿PARA QUÉ FUNCIONARA EL SITIO WEB?</h3>
             <P>Basicamente el club necesita que sus procesos de formación deportiva sean presentados de una forma novedosa 
                 y qué mejor manera de hacerlo que con su propio sitio web, además el crecimineto del club requiere que sus procesos 
                 de pago de matriculas e inscripción sean adecuados de una forma virtual. </P>
             </article>
-            <hr>
-        </div>
-        <div class="emb">
-        <iframe class="facebook" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ffenixgirardota2015&tabs=timeline&width=500&height=300&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="500" height="300" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>      
-    </div>
+            
+        </section>
+        <hr style="width: 80%; position: center; color: whitesmoke;"></hr>
+
+        <section class="destacado">
+            <h2 style="text-align: center; color: rgba(136, 133, 133, 0.904) ; font-size:xx-large;">NUESTROS PROFES</h2> <br> <br>
+            <article class="profes">
+                <img src="imagenes/profeuno.jpg" style="width:50%;
+                height:200px;" >
+                <p>
+                   <h2> Él es el profe Juanda</h2> <br>
+                     Es el encargado de la categoría Sub 5, Sub 11 y Sub 12 <br>
+                     Su nombrecompleto es Juan David Ospina Vila y tiene 28 años de edad.
+                    <h3> Sus estudios son:</h3>
+
+                    - Profesional en deporte del PCJIC <br>
+                    - Especialista en entrenamiento deportivo con énfasis en planificación y periodización de la UdeA <br>
+                    - Diplomado en docencia universitaria PCJIC <br>
+                    - Curso en el desarrollo de las manifestaciones de la fuerza y la hipertrofia muscular UdeA <br>
+                    - Curso en fisiología del envejecimiento y prescripción de actividad física en el adulto mayor UdeA<br>
+                    - Curso de rumba aeróbica UdeA<br>
+                  <h3>Su experiencia es:</h3>
+                    - Profesor del club deportivo Alianza Bello <br>
+                    - Profesor academia de Fútbol Total Itagüí <br>
+                    - Preparador físico selección Girardota de fútbol categoría Primera A <br>
+                    - Instructor del grupo de la salud del Inder de Girardota <br>
+                    - Profesor de educación fisica en sectores rurales de Girardota <br>
+                    - Metodólogo deportivo del Inder de Girardota <br>
+                    </p>
+            </article>
+            <article class="profes">
+                <img src="imagenes/profedos.jpg" style="width:50%;
+                height:200px;" >
+                <p>
+                    <h2>Él es el Profe Pipe, </h2>
+                    Es el fundador del CLUB DEPORTIVO FÉNIX y la persona encargada de Coordinar nuestro Club, acompañando los procesos deportivos de nuestros campeones
+
+                    Su nombre completo es Felipe Madrid Zapata. Tiene 26 años de edad.
+                    <h3> Sus estudios son:</h3>:
+                    -Profesional en deporte del PCJIC <br>
+                    - Diplomado en entretenimiento personalizado de la universidad de Antioquia <br>
+                     -Curso de manifestaciones de la fuerza y entrenamiento de la hipertrofia en la U de A <br>
+                      -Curso de bicicleta de estudio bajo techo en la U de A <br>
+                       - Curso de pilates en la U de A <br>
+                       Participe en el primer curso de entremiento de arqueros de indeportes Antioquia <br>
+                       <h3>Su experiencia es:</h3>
+                   - Realizó las prácticas EPM como instructor en gimnaso,
+                   - Instructor de fútbol de salón
+                   - Instructor del grupo de la salud del Inder de Girardota <br>
+                
+                </p>
+            </article>
+            <article class="profes">
+                <img src="imagenes/profetres.jpg" style="width:50%;
+                height:200px;" >
+                <p>
+                    <h2>El es el profe Jeffer</h2>
+                    Es el encargado de la categoría Sub 13 y nos apoya como fisioterapeuta en el club
+                   
+                    Su nombre completo es Jefferson Ospina Castrillon
+                    Y tiene 23 años de edad
+                     <h3>Sus Estudios son: </h3>
+            
+                        -Fisioterapia en formación <br>
+                        -Certificación en vendajes neuromusculares <br>
+                    
+                 <h3>Su experiencia es:</h3>
+
+                  -Profesor en Escuela de formación ESFUGIR <br>
+                  -Instructor de fútbol de salón <br>
+                    </p>
+            </article>
+</section>
+<hr style="width: 80%; position: center; color:whitesmoke;"></hr>
+
 
     <div class="videos">
         <h3>ALGUNOS EJERCICIOS PARA QUE PRACTIQUES EN CASA</h3>
-        <iframe class="vid" width="800" height="350" src="https://www.youtube.com/embed/S-XUIiKrjrs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        <iframe class="vid" width="800" height="350" src="https://www.youtube.com/embed/fw4xbj54UnQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> 
-
+        <iframe class="vid" width="400" height="350" src="https://www.youtube.com/embed/S-XUIiKrjrs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe class="vid" width="400" height="350" src="https://www.youtube.com/embed/fw4xbj54UnQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> 
+        <iframe class="vid" width="400" height="350" src="https://www.youtube.com/embed/SzdgMJUSE-c" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> 
+        
     </div>
         
-        <footer>
-         
-        <h4>Datos de contacto</h4>
-            <ul>
-                <li>celular: 314 8461524</li>
-                <li><a href="https://www.facebook.com/fenixgirardota2015/" target="blank"> https://www.facebook.com/fenixgirardota2015/</a></li>
-                <li>correo: fenixgirardota@gmail.com</a></li>
-            </ul>
- 
- 
-        </footer>
+
+        <footer>   
+            <hr style="height: 0px;border:1px dashed;">
+            
+            <article>
+<p>
+    <h3 style="margin-top: 80px;font-weight:bolder">¿NECESITAS AYUDA?</h3>
+Politicas de privacidad <br><br>
+Términos y condiciones
+</p>
+ </article>   
+
+ <article style="margin-top:80px;font-weight:bolder ;">
+     <P>
+<h3 >SOBRE NOSOTROS</h3>
+Quienes somos <br> <br>
+<a  style="text-decoration: none;" href="https://www.facebook.com/fenixgirardota2015/" target="blank"> Siguenos en facebook</a> <br><br>
+<a style="text-decoration: none;" href="https://www.youtube.com/channel/UCyOHtjNnCvPC4cJNmcHEZZw/featured" target="blank" > Siguenos en youtube </a> <br>
+</P>
+ </article>
+            <hr style="margin-top: 400px;height: 0px;border:1px dashed;">       
+                   
+                </footer>
+
+             
 </body>
-
 </html>
-
